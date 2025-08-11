7eaaeb7f7ab0b18a3dad63ed727bdf12db435b26
Nikolay-Kotelevets <nikolay.kotelevets@scrumlaunch.com>
2025-08-11 23:29:27 +0300

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text } from "react-native";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
@@ -17,13 +22,40 @@ export default function RootLayout() {
     return null;
   }
 
+  const handleLogoutPress = () => {
+    console.log("Logout");
+  };
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerTitle: "",
+          headerRight: () => (
+            <Pressable
+              onPress={handleLogoutPress}
+              style={styles.logoutContainer}
+            >
+              <Text style={styles.logout}>Logout</Text>
+            </Pressable>
+          ),
+        }}
+      >
+        <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  logoutContainer: {
+    height: 30,
+  },
+  logout: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "red",
+  },
+});
